fd53cffc6af2a515b3d26637cf04f18bc0ae078d
kubo455 <jakub.g455@gmail.com>
2024-12-25 19:36:39 +0100

    Adding user authetication

--- my_app/views.py
+++ my_app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Index Page !!!!")
+    print(request.user.is_authenticated)
+    if request.user.is_authenticated:
+        return render(request, 'my_app/index.html')
+        
+    return render(request, 'my_app/login.html')
+
+def login_view(request):
+    return render(request, 'my_app/login.html')
+
+def reqister_view(request):
+    return render(request, 'my_app/register.html')
