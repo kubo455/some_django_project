83c50b76708213b25330b2314b0b314523f96654
kubo455 <jakub.g455@gmail.com>
2024-12-26 14:28:29 +0100

    Login form added

--- random_project/my_app/views.py
+++ random_project/my_app/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Index Page !!!!")
+    print(request.user.is_authenticated)
+    if request.user.is_authenticated:
+        return render(request, 'my_app/index.html')
+        
+    return render(request, 'my_app/login.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            print("Loged in")
+            login(request, user)
+            return render(request, 'my_app/index.html')
+        else:
+            print("ERRROR")
+            return render(request, 'my_app/login.html', {'message':  'Invalid username or password'})
+
+    return render(request, 'my_app/login.html')
+
+def register_view(request):
+    return render(request, 'my_app/register.html')
+
+def logout_view(request):
+    logout(request)
+    return render(request, "my_app/login.html")
