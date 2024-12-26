4ef29f51be61c9fe13a3b0b2f0cf9a1ce4c86959
kubo455 <jakub.g455@gmail.com>
2024-12-26 17:32:53 +0100

    Register page changes and login changes

--- my_app/views.py
+++ my_app/views.py
@@ -1,18 +1,58 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth import authenticate
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+from .models import User
 
 def index(request):
-    print(request.user.is_authenticated)
+    # My home page
     if request.user.is_authenticated:
         return render(request, 'my_app/index.html')
         
     return render(request, 'my_app/login.html')
 
 def login_view(request):
+    if request.user.is_authenticated:
+        return render(request, 'my_app/index.html')
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        # Check if the password or username is right
+        else:
+            return render(request, 'my_app/login.html', {'message':  'Invalid username or password'})
+
     return render(request, 'my_app/login.html')
 
-def reqister_view(request):
+def register_view(request):
+    if request.user.is_authenticated:    
+        return render(request, 'my_app/index.html')
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_conf = request.POST['password-confirmation']
+        if password != password_conf:
+            return render(request, 'my_app/register.html', {'message': 'Passwords do not match'})
+        else:
+            # Check if the username exits
+            check_new_user = User.objects.filter(username=username).exists()
+            if check_new_user:
+                return render(request, 'my_app/register.html', {'message': 'User with this name already exist'})
+            else:
+                new_user = User.objects.create(username=username)
+                new_user.set_password(password)
+                new_user.save()
+                login(request, new_user)
+                return HttpResponseRedirect(reverse('index'))
+
     return render(request, 'my_app/register.html')
+
+def logout_view(request):
+    logout(request)
+    return render(request, "my_app/login.html")
