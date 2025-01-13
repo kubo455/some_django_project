25dff97d3d000de306c10c0d39415005b6f761ca
kubo455 <jakub.g455@gmail.com>
2025-01-13 18:34:16 +0100

    Changes to search book page style

--- my_app/static/my_app/search_book.js
+++ my_app/static/my_app/search_book.js
@@ -1,29 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    document.querySelector("#book-view").style.display = 'none'; 
+    // document.querySelector("#book-view").style.display = 'none'; 
     searchBook();
 })
 
 function searchBook() {
     
     document.querySelector('#search-book').onclick = function() {
-        document.querySelector("#book-view").style.display = 'block'; 
+        // document.querySelector("#book-view").style.display = 'block'; 
 
-        fetch(`https://openlibrary.org/search.json?q=${document.querySelector('#title').value}`)
+        fetch(`https://openlibrary.org/search.json?q=${document.querySelector('#title').value}&limit=10`)
         .then(response => response.json())
         .then(data => {
             console.log(data.docs);
 
             data.docs.forEach(book => {
                 const element = document.createElement('div');
-                element.classList.add('row', 'border', 'rounded', 'py-3');
-
-                element.innerHTML = `<div class="col">
-                                        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img rounded-start" alt="..." style="width: 125px; height: auto;">
+                element.classList.add('row', 'border', 'rounded', 'p-3', 'mt-3');
+                // element.style.height = '150px';
+                
+                element.innerHTML = `<div>
+                                        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img rounded-start" alt="..." style="width: 60px; height: 90px;">
                                     </div>
-                                    <div class="col">
-                                        <h5 class="card-title">${book.title}</h5>
+                                    <div class="ml-3">
+                                        <h6 class="card-title" style="font-weight: bold;">${book.title}</h6>
                                         <p class="card-text">${book.author_name[0]}</p>
+                                    </div>
+                                    <div class="col d-flex justify-content-end align-items-start">
+                                        <button class="btn btn-primary">Add to books</button>
                                     </div>`;
 
                 document.querySelector("#book-view").append(element);
